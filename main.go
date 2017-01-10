4fc6eecedc145b042f05275ef68af0de19c04138
Chase Starr <chasestarr@gmail.com>
2017-01-09 23:57:15 -0800

    refactor to use goroutines...
    it's pretty heavy on my computer, it got through about half
    of the folders and crashed

--- main.go
+++ main.go
@@ -7,8 +7,26 @@ import (
 	"os"
 	"os/exec"
 
+	"sync"
+
 	"github.com/chasestarr/fsdft"
 )
+
+type worker struct {
+	cmd  string
+	args []string
+}
+
+func (w worker) run(wg *sync.WaitGroup) {
+	fmt.Println("in run func")
+	if err := exec.Command(w.cmd, w.args...).Run(); err != nil {
+		fmt.Println("oh no!")
+		log.Fatal(err)
+		defer wg.Done()
+	}
+	fmt.Println("merged file at:", w.args[len(w.args)-1])
+	defer wg.Done()
+}
 
 func readDir(root string) []os.FileInfo {
 	files, err := ioutil.ReadDir(root)
@@ -21,6 +39,9 @@ func readDir(root string) []os.FileInfo {
 func getPdfs(root string, files []os.FileInfo) []string {
 	pdfs := []string{}
 	for _, file := range files {
+		if file.Name() == os.Args[2] {
+			continue
+		}
 		if isPdf(file) {
 			path := root + "/" + file.Name()
 			pdfs = append(pdfs, path)
@@ -29,7 +50,7 @@ func getPdfs(root string, files []os.FileInfo) []string {
 	return pdfs
 }
 
-func merge(root string, file os.FileInfo) {
+func collect(root string, file os.FileInfo, c chan worker) {
 	path := root + "/" + file.Name()
 	if file.IsDir() && isLeafDir(path) {
 		files := readDir(path)
@@ -43,11 +64,7 @@ func merge(root string, file os.FileInfo) {
 			output := path + "/" + os.Args[2]
 			args = append(args, output)
 
-			if err := exec.Command(cmd, args...).Run(); err != nil {
-				fmt.Println("oh no!")
-				log.Fatal(err)
-			}
-			fmt.Println("merged file at:", output)
+			c <- worker{cmd: cmd, args: args}
 		}
 	}
 }
@@ -80,5 +97,19 @@ func isPdf(file os.FileInfo) bool {
 // /Users/chasestarr/Dropbox/hr/job-search
 func main() {
 	root := os.Args[1]
-	fsdft.DFT(root, merge)
+	c := make(chan worker)
+	go func() {
+		fsdft.DFT(root, func(root string, file os.FileInfo) {
+			collect(root, file, c)
+		})
+		defer close(c)
+	}()
+
+	var wg sync.WaitGroup
+	for w := range c {
+		wg.Add(1)
+		go w.run(&wg)
+	}
+
+	wg.Wait()
 }
